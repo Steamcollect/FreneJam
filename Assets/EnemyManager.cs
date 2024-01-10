322d77fcd2ba2ac7ccaeb67949fb0f30e5946912
Steamcollect <eliot.garnier.080805@gmail.com>
2024-01-10 22:56:38 +0100

    Enemy_1

---
+++ Assets/EnemyManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public List<EnemyController> enemys;
+
+    Vector2 velocity = Vector2.zero;
+
+    GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < enemys.Count; i++)
+        {
+            if (enemys[i] != null)
+            {
+                float distanceFromPlayer = Vector2.Distance(enemys[i].transform.position, player.transform.position);
+
+                Vector2 lookDir = player.transform.position - enemys[i].transform.position;
+                float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+                enemys[i].transform.rotation = Quaternion.Euler(0, 0, angle);
+
+                if (distanceFromPlayer > enemys[i].attackRange)
+                {
+                    enemys[i].transform.position = Vector2.SmoothDamp(enemys[i].transform.position, player.transform.position, ref velocity, distanceFromPlayer / enemys[i].moveSpeed);
+                }
+            }
+            else
+            {
+                enemys.Remove(enemys[i]);
+                i -= 1;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/BulletManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    public List<Bullet> bullets;
+
+    GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < bullets.Count; i++)
+        {
+            if (bullets[i] != null)
+            {
+                if(Vector2.Distance(bullets[i].transform.position, player.transform.position) > 25)
+                {
+                    Destroy(bullets[i].gameObject);
+                    bullets.Remove(bullets[i]);
+                    i -= 1;
+                }
+                else bullets[i].rb.velocity = bullets[i].transform.up * bullets[i].moveSpeed;
+            }
+            else
+            {
+                bullets.Remove(bullets[i]);
+                i -= 1;
+            }
+        }
+    }
+}
